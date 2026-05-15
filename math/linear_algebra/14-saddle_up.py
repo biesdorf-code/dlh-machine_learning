ceb71d59e4fbb5563549ad0f549d05eb11f0ac72
Fernando Biesdorf <biesdorf@gmail.com>
2026-05-15 15:21:52 +0200

    14. matmul is same as @, returns a ndarray.

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for matmul with np."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """MAT MULtiplication of ndarrays, returns a new nsarray."""
+    return np.matmul(mat1, mat2)
