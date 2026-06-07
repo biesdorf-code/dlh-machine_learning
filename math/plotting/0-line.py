922f5f10714a1150694c393c5486ae8430b65834
Fernando Biesdorf <biesdorf@gmail.com>
2026-06-07 11:30:35 +0200

    0. documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+""" draws a line """
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
+    """"draws a test redline"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
