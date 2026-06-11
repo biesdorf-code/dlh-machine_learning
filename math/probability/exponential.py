5fabee37ec964ce42e5ebfebba8a9ce85446b7c5
Fernando Biesdorf <biesdorf@gmail.com>
2026-06-11 12:42:23 +0200

    4. Exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -5,6 +5,8 @@
 class Exponential:
     """an Exponential distribution"""
 
+    e = 2.7182818285  # given at project description
+
     def __init__(self, data=None, lambtha=1.):
         """initialize the instance"""
         if data is None:
@@ -17,3 +19,18 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(1 / (sum(data) / len(data)))
+
+    def pdf(self, x):
+        """calculates the value of the PDF for a given time period"""
+
+        # PDF - Probability Density Function, Density is for continuous values
+
+        if x < 0:
+            return 0
+
+        e_to_the_neg_lambtha_x = Exponential.e ** (-self.lambtha * x)
+        pdf_value = self.lambtha * e_to_the_neg_lambtha_x
+
+        # le
+
+        return pdf_value
