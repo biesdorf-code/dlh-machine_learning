6bfe88aeb065fbf95c101affbd7f1434fcc2d66f
Fernando Biesdorf <biesdorf@gmail.com>
2026-06-13 10:18:03 +0200

    11. Binomial with PMF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -36,3 +36,38 @@ class Binomial:
 
             # recalculate p
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        """calculates the value of the PMF for a given number of successes"""
+
+        # PMF - prob. of getting exactly k successes out of n trials
+        # P(k) = C(n, k) * p^k * (1-p)^(n-k)
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0 or k > self.n:  # Cant have more success than trials
+            return 0
+
+        # C(n, k) = n! / (k! * (n-k)!), built from hand-rolled factorials
+        n_factorial = 1  # 0! = 1
+        for i in range(1, self.n + 1):
+            n_factorial *= i
+
+        k_factorial = 1  # 0! = 1
+        for i in range(1, k + 1):
+            k_factorial *= i
+
+        n_minus_k_factorial = 1  # 0! = 1
+        for i in range(1, self.n - k + 1):
+            n_minus_k_factorial *= i
+
+        # factorials built, now the Bernoulli math
+        # binomial coefficient represent the ways I can win, order of luck.
+
+        binomial_coefficient = n_factorial / \
+            (k_factorial * n_minus_k_factorial)
+
+        success_part = self.p ** k
+        failure_part = (1 - self.p) ** (self.n - k)
+
+        pmf_value = binomial_coefficient * success_part * failure_part
+        return pmf_value
