3ebe111fa69a02b6ad9ce4e25f8016595fe02f39
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-09 16:10:22 +0200

    t2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""loads data from a file into a pd.DataFrame
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """loads data from a file as a pd.DataFrame
+
+    Returns: the loaded pd.DataFrame
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
