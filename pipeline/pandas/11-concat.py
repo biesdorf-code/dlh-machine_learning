6ee4327912932d2ea49ff74733f796bc30ca6456
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-09 17:31:24 +0200

    t11, create multiIndexes while contating

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""concatenates coinbase and bitstamp dataframes indexed by Timestamp
+"""
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """indexes both dataframes on Timestamp
+
+    Returns: the concatenated pd.DataFrame
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2[df2.index <= 1417411920]
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    return df
