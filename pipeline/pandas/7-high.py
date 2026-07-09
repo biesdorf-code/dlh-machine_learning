9c88f75c7cbf44b8ff5eebc9a76ba67a264aaf4e
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-09 16:59:00 +0200

    t7, .sort_values

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""sorts a pd.DataFrame by the High price in descending order
+"""
+import pandas as pd
+
+
+def high(df):
+    """sorts the pd.DataFrame by High price, descending
+
+    Returns: the sorted pd.DataFrame
+    """
+    df = df.sort_values(by="High", ascending=False)
+    return df
