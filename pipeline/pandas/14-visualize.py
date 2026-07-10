6c4c7001f1b52526cb85399482b73403ed3ebb1f
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-10 12:34:43 +0200

    t14, comments, and fixed start date

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-
+''' organizes the data, and plots it. 
+'''
 import matplotlib.pyplot as plt
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# YOUR CODE HERE
+# remove, rename, change shape
 df = df.drop(columns=["Weighted_Price"])
 df = df.rename(columns={"Timestamp": "Date"})
 df["Date"] = pd.to_datetime(df["Date"], unit="s")
 df = df.set_index("Date")
 
+# data quality upgrade
 df["Close"] = df["Close"].ffill()
 df["High"] = df["High"].fillna(df["Close"])
 df["Low"] = df["Low"].fillna(df["Close"])
@@ -19,7 +21,10 @@ df["Open"] = df["Open"].fillna(df["Close"])
 df["Volume_(BTC)"] = df["Volume_(BTC)"].fillna(0)
 df["Volume_(Currency)"] = df["Volume_(Currency)"].fillna(0)
 
+df = df[df.index >= '2017-01-01']  # keep data after 1st Jan 2017
 
+# downsampling from minute to days,
+# The agg()regates the columns accordign to stat. funnctions
 daily = df.resample("D").agg({
     "High": "max",
     "Low": "min",
@@ -29,7 +34,7 @@ daily = df.resample("D").agg({
     "Volume_(Currency)": "sum"
 })
 
-print(daily)
+print(daily)  # yes, just to the standartd output
 
-daily.plot()
-plt.show()
+daily.plot()  # prep the plot
+plt.show()  # show the plot
