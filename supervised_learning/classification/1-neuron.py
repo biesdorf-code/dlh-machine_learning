5b29799003a57b60be20b6bda4c9eb8f0ecc4076
Fernando Biesdorf <biesdorf@gmail.com>
2026-08-09 16:16:21 +0200

    task1, neuron class

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" defines a single neuron performing binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """ a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize the neuron with nx input features."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """The weights vector for the neuron."""
+        return self.__W
+
+    @property
+    def b(self):
+        """The bias for the neuron."""
+        return self.__b
+
+    @property
+    def A(self):
+        """The activated output of the neuron."""
+        return self.__A
