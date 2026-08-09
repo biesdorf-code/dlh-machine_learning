04c85888af866ad5b693cdb4292de67d42815f34
Fernando Biesdorf <biesdorf@gmail.com>
2026-08-09 16:52:37 +0200

    t16, flex number of layers

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" defines a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Initialize the network with nx features and a list of layers."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        prev = nx
+        for i in range(self.L):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights["W{}".format(i + 1)] = (
+                np.random.randn(layers[i], prev) * np.sqrt(2 / prev))
+            self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+            prev = layers[i]
