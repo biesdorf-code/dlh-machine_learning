19d10986617a2664eb8a2e5cb4152474cc8731c5
Fernando Biesdorf <biesdorf@gmail.com>
2026-08-12 16:45:26 +0200

    t0 and readme

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" builds a neural network with the keras library """
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ builds a neural network with the keras library """
+    model = K.Sequential()
+    regularizer = K.regularizers.l2(lambtha)
+
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i],
+                                     activation=activations[i],
+                                     kernel_regularizer=regularizer,
+                                     input_shape=(nx,)))
+        else:
+            model.add(K.layers.Dense(layers[i],
+                                     activation=activations[i],
+                                     kernel_regularizer=regularizer))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+
+    return model
---
+++ supervised_learning/keras/README.md
@@ -0,0 +1,1 @@
+Building, training, and saving neural networks with TensorFlow 2 and Keras.
