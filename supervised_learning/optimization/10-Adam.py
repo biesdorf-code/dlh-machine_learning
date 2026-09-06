e64cbcd5dd523177a78cc37a74e08bc60f6549f1
Fernando Biesdorf <biesdorf@gmail.com>
2026-09-06 22:46:59 +0200

    t10, adam

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" sets up the Adam optimization algorithm in TensorFlow """
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """ sets up the Adam optimization algorithm in TensorFlow """
+    return tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=beta1,
+                                    beta_2=beta2, epsilon=epsilon)
