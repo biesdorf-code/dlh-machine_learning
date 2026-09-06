4c06309e055e85426f7f938c6770380f10362900
Fernando Biesdorf <biesdorf@gmail.com>
2026-09-06 22:51:17 +0200

    t11, lr decay

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" updates the learning rate using inverse time decay """
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """ updates the learning rate using inverse time decay """
+    return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
