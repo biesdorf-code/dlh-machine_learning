33958a85b32cd744f44fedcaa0bc409369a5d75a
Fernando Biesdorf <biesdorf@gmail.com>
2026-09-23 14:37:27 +0000

    fixed trainling spaces

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -9,9 +9,9 @@ def pca_color(image, alphas):
     flat = tf.reshape(img, [-1, 3])  # one long strip of pixels
     flat = flat - tf.reduce_mean(flat, axis=0)  # grayish, dark, odd tints
     n = tf.cast(tf.shape(flat)[0] - 1, tf.float32)  # just a number
-    cov = tf.matmul(flat, flat, transpose_a=True) / n  
-    eig_vals, eig_vecs = tf.linalg.eigh(cov)  
+    cov = tf.matmul(flat, flat, transpose_a=True) / n
+    eig_vals, eig_vecs = tf.linalg.eigh(cov)
     scaled = tf.cast(alphas, tf.float32) * eig_vals
-    delta = tf.linalg.matvec(eig_vecs, scaled)  # a single faint tint
-    out = tf.clip_by_value(img + delta, 0.0, 1.0)  # dog, slightly tinted
-    return tf.cast(out * 255.0, tf.uint8)  # same tinted dog, for display
+    delta = tf.linalg.matvec(eig_vecs, scaled)
+    out = tf.clip_by_value(img + delta, 0.0, 1.0)
+    return tf.cast(out * 255.0, tf.uint8)
